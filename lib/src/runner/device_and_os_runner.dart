aa8e52f9153b32b6ef04ce013fdca7dd4b20f898
GianClyde <giannanquil17@gmail.com>
2026-02-10 13:51:13 +0800

    add debugprints in DOS Runner

--- lib/src/runner/device_and_os_runner.dart
+++ lib/src/runner/device_and_os_runner.dart
@@ -1,4 +1,5 @@
 import 'package:app_compat_benchmark_core/src/helpers/device_info_mapper.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_device_info_plus/flutter_device_info_plus.dart';
 import 'package:app_compat_benchmark_core/src/models/device_and_os/device_information.dart'
     as core;
@@ -8,6 +9,7 @@ class DeviceAndOsRunner {
 
   Future<core.DeviceInformation> getDeviceInformation() async {
     final pluginInfo = await _plugin.getDeviceInfo();
+    debugPrint("DeviceAndOsRunner pluginInfo $pluginInfo");
     return DeviceInformationMapper.fromPlugin(pluginInfo);
   }
 }
